d543a62c86fe48accd216ca21b3684041e1a35dd
wytee64 <raplowwytee@gmail.com>
2026-07-29 17:55:55 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+print("Hello World")
+
+git init .
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/wytee64/indaba-hackathon-project.git
+git push -u origin main
